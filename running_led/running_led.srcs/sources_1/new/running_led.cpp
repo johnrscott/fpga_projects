67f27070d3ffaa21656a900436b42ba049a2f530
John Scott <johnrscott0@gmail.com>
2024-01-11 22:11:13 +0000

    Got absic verilator example working

--- running_led/running_led.srcs/sources_1/new/running_led.cpp
+++ running_led/running_led.srcs/sources_1/new/running_led.cpp
@@ -5,4 +5,13 @@
 int main(int argc , char **argv) {
     Verilated::commandArgs(argc, argv);
     Vrunning_led *tb = new Vrunning_led;
+
+    for (int k = 0; k < 20; k++) {
+	tb->clk = k&1;
+	tb->eval();
+
+	std::cout << "k = " << k << std::endl;
+	std::cout << "clk = " << (int)tb->clk << std::endl;
+	std::cout << "led[0] = " << (int)tb->leds << std::endl;
+    }
 }
