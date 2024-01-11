a79a5bd542952e54f6e0d34efeba61040d3b1f01
John Scott <johnrscott0@gmail.com>
2024-01-11 18:21:31 +0000

    Updated the running LED to use a trigger

--- running_led/running_led.srcs/sources_1/new/running_led.v
+++ running_led/running_led.srcs/sources_1/new/running_led.v
@@ -1,12 +1,17 @@
 `default_nettype none
 
 module running_led(
-  input wire clk,
-  input wire rst,
-  output reg [3:0] leds
-  );
+   input wire	    clk, rst, request,
+   output wire	    busy,
+   output reg [3:0] leds
+);
 
+   // For the formal verification, the cover checks will fail
+   // if the clock rate is too high, because too many steps are
+   // needed before anything happens. For running on a board,
+   // change back to 100 MHz.
    parameter	    CLK_RATE_HZ = 100_000_000;
+   //parameter	    CLK_RATE_HZ = 5;
 
    // Current state of the running LED
    reg [2:0]	    state = 0;
@@ -15,57 +20,85 @@ module running_led(
    reg [31:0]	    wait_counter = CLK_RATE_HZ - 1;
 
    always @(posedge clk) begin
-      if (rst || (wait_counter == 0))
+      if (rst)
 	wait_counter <= CLK_RATE_HZ - 1;
       else
-	wait_counter <= wait_counter - 1'b1;
+	// If the state is not idle, then run the
+	// wait counter
+	if (state != 0)
+	  if (wait_counter == 0)
+	    wait_counter <= CLK_RATE_HZ - 1;
+	  else
+	    wait_counter <= wait_counter - 1'b1;
    end
 
    // Update state
    always @(posedge clk) begin
       if (rst)
+	state <= 0; // Reset to idle
+      else if (request && !busy)
+	state <= 1; // Initiate request
+      else if ((state >= 7) && (wait_counter == 0))
 	state <= 0;
-      else if (wait_counter == 0)
-	if (state < 5)
-	  // LED goes up then back down
+      else
+	if (wait_counter == 0)
 	  state <= state + 1;
-	else
-	  // Final LED in pattern; reset
-	  state <= 0;
-
    end
+
+   assign busy = (state != 0);
    
    // Now determine the lit LED from the state
    always @(*) begin
       case (state)
-	3'h0: leds = 4'b0001;
-	3'h1: leds = 4'b0010;
-	3'h2: leds = 4'b0100;
-	3'h3: leds = 4'b1000;
-	3'h4: leds = 4'b0100;
-	3'h5: leds = 4'b0010;
+	3'h1: leds = 4'b0001;
+	3'h2: leds = 4'b0010;
+	3'h3: leds = 4'b0100;
+	3'h4: leds = 4'b1000;
+	3'h5: leds = 4'b0100;
+	3'h6: leds = 4'b0010;
+	3'h7: leds = 4'b0001;
 	default: leds = 0;
       endcase
    end
    
 `ifdef FORMAL
 
+   reg past_valid = 0;
    always @(posedge clk)
-     if (rst)
-       reset_works: assert((wait_counter == (CLK_RATE_HZ - 1)) && (state == 0));
+     past_valid <= 1;
+
+   always @(posedge clk)
+     if (past_valid && $past(rst))
+        reset_works: assert((wait_counter == (CLK_RATE_HZ - 1)) && (state == 0));
    
    // Check that the only valid outputs are exactly one LED
    // lit at a time
-   exactly_one_led: assert property($onehot(leds));
+   exactly_one_led: assert property($onehot0(leds));
    
    // For a check like this, notice how it fails if you remote
    // the initialisation line state = 0. This is because state
    // could begin with any value in theory, so it could violate
    // this check right at the start.
-   no_invalid_states: assert property(state < 6);
+   no_invalid_states: assert property(state < 8);
 
    // Check the counter is always in range
    wait_counter_value: assert property(wait_counter < CLK_RATE_HZ);
+
+   // Check that each LED lights at least once
+   led_0_on: cover property(leds[0] == 1);
+   led_1_on: cover property(leds[1] == 1);
+   led_2_on: cover property(leds[2] == 1);
+   led_3_on: cover property(leds[3] == 1);
+
+   // Check busy signal is always asserted when LEDs are on
+   busy_asserted: assert property((leds == 0) || busy);
+
+   // Check that the state increments while busy
+   always @(posedge clk)
+     if (past_valid && $past(busy) && ($past(wait_counter) == 0) && !$past(rst))
+       state_increment_by_one: assert (state == $past(state) + 1);
+
+   // 
    
 `endif
 		    
