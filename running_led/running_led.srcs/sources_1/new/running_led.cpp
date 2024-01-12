f7cfb2ae2e8aeb69d3409a8d2075c57d0684cb8a
John Scott <johnrscott0@gmail.com>
2024-01-12 21:49:36 +0000

    Added clock update to verilator code

--- running_led/running_led.srcs/sources_1/new/running_led.cpp
+++ running_led/running_led.srcs/sources_1/new/running_led.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 #include "Vrunning_led.h"
 #include <verilated.h>
+#include <verilated_vcd_c.h>
+
+int clock_cycle(int tick,
+	  Vrunning_led *tb,
+	  VerilatedVcdC *tfp) {    
+    tb->clk_i = 0;
+    tb->eval();
+    if (tfp) {
+	tfp->dump(tick++);
+    }
+    
+    tb->clk_i = 1;
+    tb->eval();
+    if (tfp) {
+	tfp->dump(tick++);
+	tfp->flush();
+    }
+    return tick;
+}
 
 int main(int argc , char **argv) {
     Verilated::commandArgs(argc, argv);
-    Vrunning_led *tb = new Vrunning_led;
+    Vrunning_led *tb{new Vrunning_led{}};
 
+    // Trace output
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *tfp{new VerilatedVcdC{}};
+    tb->trace(tfp, 99);
+    tfp->open("running_led_trace.vcd");
+
+    int tick{0};
     for (int k = 0; k < 20; k++) {
-	tb->clk = k&1;
-	tb->eval();
-
-	std::cout << "k = " << k
-		  << " clk = " << (int)tb->clk
-		  << " led[0] = " << (int)tb->leds << std::endl;
+	tick = clock_cycle(tick, tb, tfp);
     }
 }
