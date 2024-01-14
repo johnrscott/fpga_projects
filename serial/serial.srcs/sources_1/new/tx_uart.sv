867d19a12444553ad01ed0eccbea54c3f666ac51
John Scott <johnrscott0@gmail.com>
2024-01-14 00:40:13 +0000

    More statemachine logic for uart

--- serial/serial.srcs/sources_1/new/tx_uart.sv
+++ serial/serial.srcs/sources_1/new/tx_uart.sv
@@ -4,26 +4,58 @@ module tx_uart(
    output logic	     busy, tx
 );
 
-   // Includes start and stop bit
-   logic [9:0] tx_data
+   parameter CLOCK_RATE_HZ = 100_000_000;
+   parameter BAUD_RATE_HZ = 115_200;
+   parameter CLOCKS_PER_BAUD = CLOCK_RATE_HZ / BAUD_RATE_HZ;
+
+   logic [31:0]	baud_counter = CLOCKS_PER_BAUD - 1; // counts down, bit sent on zero
+   logic	baud_stb; // bit sent when set
+   assign baud_stb = (baud_counter == 0);
    
-   enum [4:0] { IDLE, START_BIT, DATA_BIT_[8], STOP_BIT } state;
+   // Bit 0 is mapped to tx. Initially outputs 1,
+   // then changes to 0 (start bit) when transit
+   // begins. At end, reset to all ones and bit
+   // zero becomes stop bit.
+   logic [9:0] tx_data = '1;
+   assign tx = tx_data[0];
+
+   enum [4:0] { IDLE, START_BIT, DATA[8], STOP_BIT } state;
 
    initial { busy, state } = { 1'b0, IDLE };
 
-   
+   assign busy = (state != IDLE);
    
    always_ff @(posedge clk) begin
-      if ((state == IDLE) && write) begin
+      if (write && !busy)
+	// If transmit starts, reset baud counter
+	counter <= CLOCKS_PER_BAUD - 1;
+      else if (counter > 0)
+	counter--;
+      else if (busy)
+	// If transmitting, then restart baud counter
+	counter <= CLOCKS_PER_BAUD - 1;
+   end
+   
+   always_ff @(posedge clk) begin
+      if (write && !busy) begin
 	 // Load new data to transmit
-	 tx_data[8:1] <= data;
+	 tx_data <= { data, 1'b0 };
 	 // Start a new transmission
 	 state <= START_BIT;
       end
-	 
+      else if (baud_stb) begin
+	 // Only update state on baud strobe
+	 if (busy && (state < STOP_BIT)) begin
+	    tx_data <= tx_data[9:1];
+	    state <= state + 1;
+	 end
+	 else begin
+	    // Back to idle
+	    state <= IDLE;
+	    tx_data <= '1;
+	 end
+      end // if (baud_stb)
    end
-
-   // 
-
-   
+	       
+	       
 endmodule
